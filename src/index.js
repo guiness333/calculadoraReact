21ebf89913b443397dab7a5ddda1c8d2c649be25
Walber Martins <walber123k@hotmail.com>
2020-04-22 12:40:44 -0300

    calculadora funcionando

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Calculator from './Main/Calculator';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <h1>Calculadora</h1>
+    <Calculator />
   </React.StrictMode>,
   document.getElementById('root')
 );
